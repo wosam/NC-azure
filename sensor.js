3ca33be203298d18884df86730d3a46c3488d918
Wojciech Samborski <wojciech.samborski@ad.adrem>
2020-05-21 00:57:11 +0200

    service and sensor improvements

--- sensor.js
+++ sensor.js
@@ -1,16 +1,27 @@
 const MonitorAPIService = require('./service');
-const subscriptionId = '015cdaee-3214-4384-8172-445505af2019';
+//const subscriptionId = '015cdaee-3214-4384-8172-445505af2019';
 const tenant = 'e36f7ecd-3289-4db2-a23f-6b587e569a88';
 const clientId = '9513aaf3-685b-45ec-a016-b0f19151ecc7';
 const clientSecret = '1J3uN@0Isx4RFj-BrRrVnGOns/rqVe:5';
 
+const getCredentials = () => ({
+    tenant,
+    clientId,
+    clientSecret
+});
+
 async function test() {
-    MonitorAPIService.setCredentials(tenant,clientId,clientSecret);
-    const resources = await MonitorAPIService.sendMonitorAPIRequest(subscriptionId, 'resourceRequest', '', ['functionapp','Microsoft.Compute/virtualMachines']);
+    const subscription = await MonitorAPIService.sendMonitorAPIRequest(
+        getCredentials(), 'subscription');
+    console.log('SUB', subscription[0].subscriptionId);
+    const resources = await MonitorAPIService.sendMonitorAPIRequest(
+        getCredentials(), 'resource', subscription[0].subscriptionId, ['functionapp', 'Microsoft.Compute/virtualMachines']);
     console.log(resources, resources.length);
-    const metricDefinitions = await MonitorAPIService.sendMonitorAPIRequest(resources[0], 'metricDefinitionRequest');
+    const metricDefinitions = await MonitorAPIService.sendMonitorAPIRequest(
+        getCredentials(), 'metricDefinition', resources[0]);
     console.log(metricDefinitions);
-    const metricValues = await MonitorAPIService.sendMonitorAPIRequest(resources[0], 'metricValuesRequest', `${metricDefinitions[0]}${metricDefinitions[2]}`);
+    const metricValues = await MonitorAPIService.sendMonitorAPIRequest(
+        getCredentials(), 'metricValues', resources[0], [], `${metricDefinitions[0]}${metricDefinitions[2]}`);
     console.log(metricValues[1]/*.timeseries[0].data*/);
 }
 
--- service.js
+++ service.js
@@ -3,23 +3,14 @@ const request = require('request');
 const supportedNamespaces = require('./supportedNamespaces');
 const resource = 'https://management.core.windows.net/';
 const authorityHostUrl = 'https://login.microsoftonline.com';
-
-let serviceClientId;
-let serviceClientSecret;
-let authContext;
 let session;
 
-const setCredentials = (tenant, clientId, clientSecret) => {
-    serviceClientId = clientId;
-    serviceClientSecret = clientSecret;
-    authContext = new adal(authorityHostUrl + '/' + tenant);
-};
-
 const getRequestSettings = (id, token, requestType, metricList) => {
-    metricList = metricList ? `metricnames=${metricList}&` : '';
+    if (metricList !== '') metricList = `metricnames=${metricList}&`;
     let url = 'https://management.azure.com' + id + '/providers/microsoft.insights/metrics?' + metricList + 'api-version=2018-01-01';
-    if (requestType === 'resourceRequest') url = 'https://management.azure.com/subscriptions/' + id + '/resources?api-version=2019-10-01';
-    if (requestType === 'metricDefinitionRequest') url = 'https://management.azure.com' + id + '/providers/microsoft.insights/metricDefinitions?api-version=2018-01-01';
+    if (requestType === 'subscription') url = 'https://management.azure.com/subscriptions?api-version=2020-01-01';
+    if (requestType === 'resource') url = 'https://management.azure.com/subscriptions/' + id + '/resources?api-version=2019-10-01';
+    if (requestType === 'metricDefinition') url = 'https://management.azure.com' + id + '/providers/microsoft.insights/metricDefinitions?api-version=2018-01-01';
 
     return {
         'method': 'GET',
@@ -32,16 +23,20 @@ const getRequestSettings = (id, token, requestType, metricList) => {
 
 const filterResources = (resourcesArray, resourceTypes = []) => {
     return resourcesArray.filter(resource => {
-        return supportedNamespaces.some(namespace => resource.id.toLowerCase().includes(namespace.toLowerCase()))
-            && !resource.id.includes('extensions')
-            && (resourceTypes ? resourceTypes.some(resourceType => resourceType === resource.type) || resourceTypes.some(resourceType => resourceType === resource.kind) : true)
+        const isNamespaceSupported = supportedNamespaces.some(namespace => resource.id.toLowerCase().includes(namespace.toLowerCase()));
+        const isResourceExtension = resource.id.includes('extensions');
+        const isResourceInResourceTypes = resourceTypes ?
+            resourceTypes.some(resourceType => resourceType === resource.type) || resourceTypes.some(resourceType => resourceType === resource.kind)
+            : true;
+        return isNamespaceSupported && !isResourceExtension && isResourceInResourceTypes
     })
 };
 
-async function getValidToken() {
+const getValidToken = (tenant, clientId, clientSecret) => {
+    const authContext = new adal(authorityHostUrl + '/' + tenant);
     if (!session || session.expiresOn < new Date()) {
         return new Promise((resolve, reject) => {
-            authContext.acquireTokenWithClientCredentials(resource, serviceClientId, serviceClientSecret, (error, tokenResponse) => {
+            authContext.acquireTokenWithClientCredentials(resource, clientId, clientSecret, (error, tokenResponse) => {
                 if (error) resolve(error);
                 session = tokenResponse;
                 resolve(session);
@@ -49,24 +44,23 @@ async function getValidToken() {
         });
     }
     return session;
-}
+};
 
-async function sendMonitorAPIRequest(id, requestType, metricList = '', resourceTypes = []) {
-    const token = await getValidToken();
+const sendMonitorAPIRequest = async (credentials, requestType, id = '', resourceTypes = [], metricList = '',) => {
+    const token = await getValidToken(credentials.tenant, credentials.clientId, credentials.clientSecret);
     const requestSettings = getRequestSettings(id, token.accessToken, requestType, metricList);
     return new Promise((resolve, reject) => {
         request(requestSettings, (error, response) => {
             const responseBody = JSON.parse(response.body).value;
             if (error) resolve(error);
-            if (requestType === 'resourceRequest') {
-                resolve(filterResources(responseBody, resourceTypes).map(resource => resource.id))
-            }
-            if (requestType === 'metricDefinitionRequest') resolve(responseBody.map(metricDefinition => metricDefinition.name.value + ','));
-            if (requestType === 'metricValuesRequest') resolve(responseBody)
+            if (requestType === 'subscription') resolve(responseBody);
+            if (requestType === 'resource') resolve(filterResources(responseBody, resourceTypes).map(resource => resource.id));
+            if (requestType === 'metricDefinition') resolve(responseBody.map(metricDefinition => metricDefinition.name.value + ','));
+            if (requestType === 'metricValues') resolve(responseBody)
         })
     })
-}
+};
 
-module.exports = {sendMonitorAPIRequest, setCredentials};
+module.exports = {sendMonitorAPIRequest};
 
 
